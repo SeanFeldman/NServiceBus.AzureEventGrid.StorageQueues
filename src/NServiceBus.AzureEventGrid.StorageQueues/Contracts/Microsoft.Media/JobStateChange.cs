9f94fad6734ae15a8d6eb8247610844ba99a4125
Sean Feldman <feldman.sean@gmail.com>
2018-07-23 00:03:39 -0600

    Microsoft.Media event

---
+++ src/NServiceBus.AzureEventGrid.StorageQueues/Contracts/Microsoft.Media/JobStateChange.cs
@@ -0,0 +1,17 @@
+﻿namespace NServiceBus.AzureEventGrid.StorageQueues.Microsoft.Media
+{
+    /// <summary>
+    /// Raised when a state of the Job changes.
+    /// </summary>
+    public class JobStateChange
+    {
+        /// <summary>
+        /// The state of the job before the event.
+        /// </summary>
+        public string PreviousState { get; set; }
+        /// <summary>
+        /// The new state of the job being notified in this event. For example, "Queued: The Job is awaiting resources" or “Scheduled: The job is ready to start".
+        /// </summary>
+        public string State { get; set; }
+    }
+}
