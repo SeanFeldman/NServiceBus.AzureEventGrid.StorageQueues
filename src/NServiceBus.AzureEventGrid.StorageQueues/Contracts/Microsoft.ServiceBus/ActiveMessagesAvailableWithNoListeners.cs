b722ea72b3ddc843adb2a29ea12715ed16c849fe
Sean Feldman <feldman.sean@gmail.com>
2018-07-23 00:17:46 -0600

    Microsoft.ServiceBus events

---
+++ src/NServiceBus.AzureEventGrid.StorageQueues/Contracts/Microsoft.ServiceBus/ActiveMessagesAvailableWithNoListeners.cs
@@ -0,0 +1,33 @@
+﻿namespace Microsoft.ServiceBus
+{
+    /// <summary>
+    /// Raised when there are active messages in a Queue or Subscription and no receivers listening.
+    /// </summary>
+    public class ActiveMessagesAvailableWithNoListeners
+    {
+        /// <summary>
+        /// The Service Bus namespace the resource exists in.
+        /// </summary>
+        public string NamespaceName { get; set; }
+        /// <summary>
+        /// The URI to the specific queue or subscription emitting the event.
+        /// </summary>
+        public string Uri { get; set; }
+        /// <summary>
+        /// The type of Service Bus entity emitting events (queue or subscription).
+        /// </summary>
+        public string EntityType { get; set; }
+        /// <summary>
+        /// The queue with active messages if subscribing to a queue. Value null if using topics / subscriptions.
+        /// </summary>
+        public string QueueName { get; set; }
+        /// <summary>
+        /// The topic the Service Bus subscription with active messages belongs to. Value null if using a queue.
+        /// </summary>
+        public string TopicName { get; set; }
+        /// <summary>
+        /// The Service Bus subscription with active messages. Value null if using a queue.
+        /// </summary>
+        public string SubscriptionName { get; set; }
+    }
+}
